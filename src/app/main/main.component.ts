9fe0cfa85cc49b7cd710aaf6678ab60fc5080eaa
ben-blb <ben.blb@live.com.mx>
2021-04-12 11:38:38 -0500

    finished main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from '../services/api.service';
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+	peopleList: any = {};
+	jobList: any = {};
+	searchQ: string = "";
 
-  constructor() { }
+	constructor(private router: Router, private api: ApiService) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.api.getPeople().subscribe(main => {
+			this.peopleList = main.results;
+		});
+
+		this.api.getAllJobs().subscribe(main => {
+			this.jobList = main.results;
+			console.log(main);
+		});
+	}
+
+	doSearch(): void{
+		this.router.navigate(['/search/' + this.searchQ]);
+	}
 
 }
